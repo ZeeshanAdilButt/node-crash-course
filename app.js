d90ab9a2d18f300d466c602ab6c0bb459f84488c
Zeeshan Adil <zeeshanadil56@gmail.com>
2020-08-20 09:15:41 +0500

    adding middlewares

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app=express();
 
@@ -6,6 +7,25 @@ app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000);
+
+
+app.use(express.static('public'));
+
+//middleware
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     
